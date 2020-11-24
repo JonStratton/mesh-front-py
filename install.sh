cace637b6c3fcb4daa0442b5cf52dd12a88b073d
jgstratton <jgstratton@meshtest1.lan>
2020-11-24 07:52:36 -0600

    ygg key cleanup

--- install.sh
+++ install.sh
@@ -98,6 +98,7 @@ install_yggdrasil()
 # https://yggdrasil-network.github.io/installation-linux-deb.html
 gpg --fetch-keys https://neilalexander.s3.dualstack.eu-west-2.amazonaws.com/deb/key.txt
 gpg --export 569130E8CA20FBC4CB3FDE555898470A764B32C9 | sudo apt-key add -
+gpg --batch --delete-keys 569130E8CA20FBC4CB3FDE555898470A764B32C9
 echo 'deb http://neilalexander.s3.dualstack.eu-west-2.amazonaws.com/deb/ debian yggdrasil' | sudo tee /etc/apt/sources.list.d/yggdrasil.list
 sudo apt-get update
 sudo apt-get install -y yggdrasil
@@ -178,9 +179,10 @@ fi
 uninstall_yggdrasil()
 {
 sudo apt-get remove --purge -y yggdrasil
-sudo rm /etc/yggdrasil.conf
 sudo rm /etc/apt/sources.list.d/yggdrasil.list
+sudo apt-key del 569130E8CA20FBC4CB3FDE555898470A764B32C9
 sudo apt-get update
+sudo rm /etc/yggdrasil.conf
 }
 
 uninstall_cjdns()
